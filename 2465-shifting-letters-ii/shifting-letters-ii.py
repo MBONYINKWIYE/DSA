0c1334058ba576c580855893a0511790a3fdffb4
Frank Philo <148916030+MBONYINKWIYE@users.noreply.github.com>
2026-01-30 11:51:22 +0200

    Time: 126 ms (10.43%) | Memory: 43.7 MB (6.75%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+        prefix = [0]* (n + 1)
+        accumulator = 0
+        for l, r, d in shifts:
+            if d != 0:
+                prefix[l] += 1
+                prefix[r + 1] -= 1
+            else:
+                prefix[l] -= 1
+                prefix[r + 1] +=1
+        print(prefix)   
+        for i in range(len(s)):
+            accumulator += prefix[i]
+            prefix[i] = accumulator
+        result = ""
+        for idx, char in enumerate(s):
+            temp = (ord(char) - ord('a') + prefix[idx]) % 26
+            result += chr(97 + temp)
+            print(temp)
+        return result
+        
