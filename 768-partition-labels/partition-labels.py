6ae9ee8dd3757d530937d2ed91c0553bfb1cacc6
Frank Philo <148916030+MBONYINKWIYE@users.noreply.github.com>
2026-02-01 22:48:50 +0200

    Time: 3 ms (88.64%) | Memory: 19.5 MB (12.62%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -7,10 +7,10 @@ class Solution:
 
         for i, char in enumerate(s):
             last_occurrence[char] = i
-        print(last_occurrence)
+    
         for i, char in enumerate(s):
             right = max(right, last_occurrence[char])
-            print(right)
+          
             if i == right:
                 result.append(right - left + 1)
                 left = i + 1
