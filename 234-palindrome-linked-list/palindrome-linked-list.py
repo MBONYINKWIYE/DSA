682dc0d666a3454973672bf30c8c2084ea97fe42
Frank Philo <148916030+MBONYINKWIYE@users.noreply.github.com>
2026-02-17 12:32:58 +0200

    Time: 31 ms (68.29%) | Memory: 42.7 MB (45.65%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, palind) -> ListNode:
+        prev = None
+        cur = right = palind
+        while cur:
+            new_node = cur.next
+            cur.next = prev
+            prev = cur
+            cur = new_node
+        return prev
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        cur = left = right = head
+        while right and right.next:
+            right = right.next.next
+            left = left.next
+
+        rev_cur = self.reverseList(left)
+        
+        while rev_cur:
+            if cur.val != rev_cur.val:
+                return False
+
+            cur = cur.next
+            rev_cur = rev_cur.next
+            # print(cur)
+            # print(rev_cur)
+        return True
+       
